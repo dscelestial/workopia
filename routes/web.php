48062b1d0e6b8a41bb06019e651ad273801bf7fe
Daniel Celestial <celestial.danielstephen@gmail.com>
2024-12-03 17:47:45 +0800

    minor updates

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,15 @@ use Symfony\Component\HttpFoundation\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return view('jobs.index', [
+        'title' => 'Jobs',
+        'jobs' => [
+            'Web Developer',
+            'Software Engineer',
+            'Database Administrator',
+            'System Analyst'
+        ],
+    ]);
+});
