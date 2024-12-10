a472275bbca4be39a4b3dafbe98905e3887aaffa
Daniel Celestial <celestial.danielstephen@gmail.com>
2024-12-10 13:52:10 +0800

    Mobile View Button Component

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,6 +1,6 @@
-@props(['url' => '/', 'icon' => 'null', 'bgClass' => "", 'hoverClass' => "", 'textClass' => ""])
+@props(['url' => '/', 'icon' => 'null', 'bgClass' => "", 'hoverClass' => "", 'textClass' => "", 'block' => "false"])
 
-<a href="{{$url}}" class="px-4 py-2 rounded hover:shadow-md transition duration-300 {{$bgClass}} {{$hoverClass}} {{$textClass}}">
+<a href="{{$url}}" class="px-4 py-2 rounded hover:shadow-md transition duration-300 {{$bgClass}} {{$hoverClass}} {{$textClass}} {{$block ? "block" : ""}}">
     @if ($icon)
         <i class="fa fa-{{$icon}}"></i>
     @endif
