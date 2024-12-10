19b8c98dd94def17d66f84ef42f783dfa4eb355d
Daniel Celestial <celestial.danielstephen@gmail.com>
2024-12-10 15:25:05 +0800

    Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{@asset('css/style.css')}}"></link>
     <title>{{ $title ?? 'Workopia' }}</title>
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
     <main class="mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
